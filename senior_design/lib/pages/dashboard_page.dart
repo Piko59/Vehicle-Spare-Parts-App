e404cce9ee3e2bd70bf78d16343b19d7fa65f10b
Piko59 <emresmz13@gmail.com>
2024-05-22 19:54:04 +0300

    Categories of car, motorcycle and bicycle parts have been listed.

--- senior_design/lib/pages/dashboard_page.dart
+++ senior_design/lib/pages/dashboard_page.dart
@@ -1,20 +1,155 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 
 class DashboardPage extends StatelessWidget {
-  final User? user = FirebaseAuth.instance.currentUser;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Dashboard'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => CarCategoriesPage()),
+                );
+              },
+              child: Text('Araba'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => MotorcycleCategoriesPage()),
+                );
+              },
+              child: Text('Motorsiklet'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => BicycleCategoriesPage()),
+                );
+              },
+              child: Text('Bisiklet'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class CarCategoriesPage extends StatelessWidget {
+  final List<String> carCategories = [
+    'Ateşleme & Yakıt',
+    'Egzoz',
+    'Elektrik',
+    'Filtre',
+    'Fren & Debriyaj',
+    'Isıtma & Havalandırma & Klima',
+    'Mekanik',
+    'Motor',
+    'Şanzıman & Vites',
+    'Yürüyen & Direksiyon',
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Dashboard'),
+        title: Text('Araba Kategorileri'),
       ),
-      body: Center(
-        child: Text(
-          'Connected: ${user?.email}',
-          style: const TextStyle(fontSize: 24),
-        ),
+      body: ListView.builder(
+        itemCount: carCategories.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(carCategories[index]),
+            onTap: () {
+              // Kategoriye tıklanınca ne olacağını burada belirleyin
+            },
+          );
+        },
+      ),
+    );
+  }
+}
+
+class MotorcycleCategoriesPage extends StatelessWidget {
+  final List<String> motorcycleCategories = [
+    'Debriyaj',
+    'Egzoz',
+    'Elektrik',
+    'Fren',
+    'Grenaj',
+    'Havalandırma',
+    'Motor',
+    'Süspansiyon',
+    'Şanzıman',
+    'Yağlama',
+    'Yakıt Sistemi',
+    'Yönlendirme',
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Motorsiklet Kategorileri'),
+      ),
+      body: ListView.builder(
+        itemCount: motorcycleCategories.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(motorcycleCategories[index]),
+            onTap: () {
+              // Kategoriye tıklanınca ne olacağını burada belirleyin
+            },
+          );
+        },
+      ),
+    );
+  }
+}
+
+class BicycleCategoriesPage extends StatelessWidget {
+  final List<String> bicycleCategories = [
+    'Gidon',
+    'Fren',
+    'Rotor',
+    'Bilya',
+    'Jant',
+    'Kadro',
+    'Çekiş Parçaları',
+    'Elektrik Parçaları',
+    'Kokpit',
+    'Pabuç',
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Bisiklet Kategorileri'),
+      ),
+      body: ListView.builder(
+        itemCount: bicycleCategories.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(bicycleCategories[index]),
+            onTap: () {
+              // Kategoriye tıklanınca ne olacağını burada belirleyin
+            },
+          );
+        },
       ),
     );
   }
