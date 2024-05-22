10567a7b7dba55128daa5f3ce5c35de2142b66c5
Piko59 <emresmz13@gmail.com>
2024-05-22 05:27:07 +0300

    App created. Added user interface section for splash screen, login page and registration page.

---
+++ senior_design/lib/pages/SplashScreen.dart
@@ -0,0 +1,56 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'login_page.dart';
+import 'package:lottie/lottie.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(
+      const Duration(seconds: 10),
+      () => Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => LoginPage()),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Lottie.asset(
+              'lib/animations/splash_animation.json', // Replace with the path to your Lottie JSON file
+              fit: BoxFit.cover,
+              width: 400, // Adjust the width and height as needed
+              height: 400,
+              repeat: true, // Set to true if you want the animation to loop
+            ),
+            const SizedBox(height: 10), // Space between animation and text
+            const Text(
+              "Spare Part App",
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+                color: Colors.blueAccent,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
