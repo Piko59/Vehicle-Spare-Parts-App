71ba3ec76727f6d9a634a542dc5c956d7f9a30ed
ancalpar <an-lcan@outlook.com>
2024-05-23 19:51:14 +0300

    .

--- senior_design/lib/pages/add_part_screen.dart
+++ senior_design/lib/pages/add_part_screen.dart
@@ -3,7 +3,6 @@ import 'package:image_picker/image_picker.dart';
 import 'package:firebase_storage/firebase_storage.dart' as firebase_storage;
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'dart:io';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'dashboard_page.dart';
 
 class AddPartScreen extends StatefulWidget {
